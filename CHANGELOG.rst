55fa749acb00317da58b7afce3687e0b25cd1996
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-03-08 21:15:42 +0100

    bump version to 0.0.1

---
+++ CHANGELOG.rst
@@ -0,0 +1,13 @@
+Changelog
+=========
+
+0.0.1
+-----
+
+Fix installation issue.
+
+
+0.0.0
+-----
+
+initial release
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 	name='filecachetools',
-	version='0.0.0',
+	version='0.0.1',
 	description="cachetools compatible persistent cache",
 	url='https://github.com/xi/filecachetools',
 	author='Tobias Bengfort',
